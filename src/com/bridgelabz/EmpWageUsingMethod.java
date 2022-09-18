684c7ac48ff3032fc5b3361fa36b8d29ea16ca9d
shweta <shwetabirajdar5872gmail.com>
2022-09-18 16:58:30 +0530

    Added: Daily Employee wage.

--- src/com/bridgelabz/EmpWageUsingMethod.java
+++ src/com/bridgelabz/EmpWageUsingMethod.java
@@ -3,17 +3,27 @@ package com.bridgelabz;
 public class EmpWageUsingMethod {
     int is_Full_Time = 1;    //Instance variables
     int is_Part_Time = 2;
-
+    int empWagePerHrs = 20;
+    int empHrs;
+    int empWage;
+    double empcheck = Math.floor(Math.random() * 10) % 2;
     void empPresent() {
-        double empcheck = Math.floor(Math.random() * 10) % 2;
         if (empcheck == is_Full_Time)
             System.out.println("Employee is present.");
         if (empcheck == is_Part_Time)
             System.out.println("Employee is absent.");
         }
+        void empWadge(){
+                if (empcheck == is_Full_Time)
+                    empHrs = 8;
+                empWage = empHrs * empWagePerHrs;
+                    System.out.println("Daily Employee wage = " + empWage);
+
+        }
         public static void main (String[]args){
-            EmpWageUsingMethod emp = new EmpWageUsingMethod();
-            emp.empPresent();
+        EmpWageUsingMethod emp = new EmpWageUsingMethod();
+          emp.empPresent();
+          emp.empWadge();
         }
     }
 
